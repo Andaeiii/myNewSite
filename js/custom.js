7b4ef70689a49682e422a69e17ab7c270b5ae2cc
Ande Caleb <andaeiii@gmail.com>
2023-03-06 13:35:19 +0100

    minor fixes

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,16 @@
-
-//
 $(document).ready(function() {
-			$('#fullpage').fullpage({
-				'verticalCentered': false,
-				'scrollingSpeed': 600,
-				'autoScrolling': false,
-				'css3': true,
-				'navigation': true,
-				'navigationPosition': 'right',
-			});
-		});
+    $('#fullpage').fullpage({
+        'verticalCentered': false,
+        'scrollingSpeed': 1000,
+        'autoScrolling': false,
+        'css3': true,
+        'navigation': true,
+        'navigationPosition': 'right',
+    });
+});
 
 // wow
-$(function()
-{
+$(function() {
     new WOW().init();
     $(".rotate").textrotator();
 })
